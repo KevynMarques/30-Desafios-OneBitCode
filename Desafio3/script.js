3ab85a9112861eef552c88254d2be116e019461b
Kevyn  Marques <92831981+KevynMarques@users.noreply.github.com>
2022-07-26 19:44:39 -0300

    Desafio 3

---
+++ Desafio3/script.js
@@ -0,0 +1,13 @@
+const reverso = (arr)=>{
+  const arrReverso = []
+
+
+ for( let i = 0; i < arr.legth; i++ )
+      {
+         arrReverso[i] = arr[arr.lenght -1]
+  }
+   
+  return arrReverso;
+}
+
+console.log(arrReverso(0,55,5,7,8,64,897,1,4684,987,87,96,5));
